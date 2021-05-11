26beaede5a2810c1959f6b46a12ce3211896f19a
Ross White <rosswhite@outlook.com>
2021-05-11 18:29:08 +0100

    Adding rest of initial schema

--- schema.sql
+++ schema.sql
@@ -4,7 +4,23 @@ CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
     primary key (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2),
+    primary key(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    primary key(id)
 );
