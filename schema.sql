7bd4ee7cb4c6c4318e20806d79d4c33591c21602
Ross White <rosswhite@outlook.com>
2021-05-11 19:01:25 +0100

    Creating foreign keys

--- schema.sql
+++ schema.sql
@@ -4,23 +4,25 @@ CREATE database employee_trackerDB;
 USE employee_trackerDB;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    department_id INT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    primary key (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    role_id INT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2),
-    primary key(id)
+    department_id INT
+    FOREIGN KEY (department_id)
+        REFERENCES department(department_id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    employee_id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
     manager_id INT,
-    primary key(id)
+    FOREIGN KEY (role_id)
+        REFERENCES role(role_id)
 );
