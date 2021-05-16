cc41a6e95dc2002e82db9605930eac4982132ba0
Ross White <rosswhite@outlook.com>
2021-05-16 10:28:57 +0100

    FIX: Adding manager as foreign key

--- schema.sql
+++ schema.sql
@@ -13,18 +13,15 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2),
     department_id INT,
-        FOREIGN KEY (department_id)
-        REFERENCES department(department_id)
-        ON DELETE CASCADE
+        FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     employee_id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
-        FOREIGN KEY (role_id)
-        REFERENCES role(role_id)
-        ON DELETE CASCADE
+        FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE
+        FOREIGN KEY (manager_id) REFERENCES (employee_id) ON DELETE SET NULL
 );
